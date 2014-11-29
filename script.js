4dfd6cf0413f25b668b1e2d8e8671e8eefba6ec7
Dhiraj Bodicherla <dhiraj.bodicherla@gmail.com>
2014-11-29 13:30:08 -0500

    login and tray magic is done 2. need to work on parsing message

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 var gui = require('nw.gui');
-// gui.Screen.Init();
-// var screens = gui.Screen.screens;
-win = gui.Window.get();
+var win = gui.Window.get();
 var clipboard = gui.Clipboard.get();
 var notifier = require('node-notifier');
 
-// var x = 0, y =  screens[0].bounds.height - screens[0].work_area.height;
-// var tvHeight = Math.floor(screens[0].work_area.height / 2);
-// var tvWidth = Math.floor(screens[0].bounds.width/2);
-
-if(localStorage.getItem('pushbullet-api') != null){
-  ID = localStorage.getItem('pushbullet-api');
-  connect();
+var api_key = localStorage.getItem('pushbullet-api');
+if($.trim(api_key)){
+  init();
+  connect(api_key);
 }else{
   showLogin();
 }
@@ -20,19 +15,22 @@ function showLogin(){
   win.resizeTo(450, 500);
   win.show();
   win.setShowInTaskbar(true);
-  // win.focus();
+  /* focus won't work for some reason */
+  // win.focus(); 
 }
 
 function saveUser(){
-  localStorage.setItem('pushbullet-api', $('#api-key').val());
+  localStorage.setItem('pushbullet-api', api_key);
 }
 
 function signOut(){
+  tray.remove();
+  tray = null;
   localStorage.removeItem('pushbullet-api');
   showLogin();
 }
 
-function connect(){
+function init(){
 
   tray = new gui.Tray({
       icon: 'images/trayicon-16x16.png'
@@ -41,7 +39,7 @@ function connect(){
   tray.menu = new gui.Menu();
 
   tray.menu.append(new gui.MenuItem({
-    label: 'Sign out',
+    label: 'sign out',
     click: function(){
       signOut();
     }
@@ -57,6 +55,31 @@ function connect(){
   win.setShowInTaskbar(false);
 }
 
+function connect(api_key){
+  websocket = new WebSocket('wss://stream.pushbullet.com/websocket/' + api_key);
+  websocket.onopen = function(e) {
+    
+  }
+  websocket.onmessage = function(message) {
+
+    notifier.notify({
+      title: message.push.title,
+      message: message.push.body,
+      sound: true,
+      wait: true 
+    }, function (err, response) {
+      
+    });
+  }
+  websocket.onerror = function(e) {
+      // messages.innerHTML += "<p>WebSocket onerror</p>";
+  }
+  websocket.onclose = function(e) {
+      win.show();
+      win.setShowInTaskbar(true);
+  }
+}
+
 $(document).ready(function(){
   $('input').on('keydown', function(e){
     if (e.which == 86) {
@@ -69,30 +92,17 @@ $(document).ready(function(){
   $('#connect-form').submit(function(e){
     e.preventDefault();
 
-    var self = this;
-
-    websocket = new WebSocket('wss://stream.pushbullet.com/websocket/' + $('#api-key').val());
-    websocket.onopen = function(e) {
-      $('#api-key').val('');
-      saveUser();
-      connect();
-    }
-    websocket.onmessage = function(e) {
-        notifier.notify({
-          title: 'My awesome title',
-          message: 'Hello from node, Mr. User!',
-          sound: true, // Only Notification Center or Windows Toasters
-          wait: true // wait with callback until user action is taken on notification
-        }, function (err, response) {
-          // response is response from notification
-        });
-    }
-    websocket.onerror = function(e) {
-        // messages.innerHTML += "<p>WebSocket onerror</p>";
-    }
-    websocket.onclose = function(e) {
-        win.show();
-        win.setShowInTaskbar(true);
-    }
+    api_key = $('#api-key').val();
+    $('#api-key').val('');
+    saveUser();
+    init();
+    connect(api_key);
   });
 });
+
+
+// gui.Screen.Init();
+// var screens = gui.Screen.screens;
+// var x = 0, y =  screens[0].bounds.height - screens[0].work_area.height;
+// var tvHeight = Math.floor(screens[0].work_area.height / 2);
+// var tvWidth = Math.floor(screens[0].bounds.width/2);
